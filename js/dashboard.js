b08708602994ca5efd74717d47cf2d6ce37d03d2
Agarcia-Hemac <agarcia@grupohemac.com.mx>
2022-11-11 20:38:56 -0600

    Inicio proyecto

---
+++ js/dashboard.js
@@ -0,0 +1,63 @@
+/* globals Chart:false, feather:false */
+
+(function () {
+  'use strict'
+
+  feather.replace()
+
+  // Graphs
+  var ctx = document.getElementById('myChart')
+  // eslint-disable-next-line no-unused-vars
+  var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [
+        'Enero',
+        'Febreo',
+        'Marzo',
+        'Abril',
+        'Mayo',
+        'Junio',
+        'Julio',
+        'Agosto',
+        'Septiembre',
+        'Octubre',
+        'Noviembre',
+        'Diciembre'
+      ],
+      datasets: [{
+        data: [
+          15,
+          25,
+          55,
+          75,
+          25,
+          5,
+          18,
+          22,
+          66,
+          13,
+          15,
+          19
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 4,
+        pointBackgroundColor: '#007bff'
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      }
+    }
+  })
+})()
